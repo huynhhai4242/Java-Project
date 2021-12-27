ab3c15c9e4d832bcdcbb0cd104ef8c734a1465cf
Huỳnh Chí Hải <47241315+huynhhai4242@users.noreply.github.com>
2021-12-27 19:09:37 +0700

    Update GoodString.java

--- GoodString.java
+++ GoodString.java
@@ -1,9 +1,9 @@
+//Chuyển đổi chữ in hoa về dạng thường
 package goodstring;
 
 import java.util.Scanner;
 import static java.lang.System.out;
 
-//Chuyển đổi chữ in hoa về dạng thường
 public class GoodString {
 
     public static void main(String[] args) {
@@ -16,21 +16,22 @@ public class GoodString {
         Scanner sc = new Scanner(System.in);
         out.print("Input your string: ");
         String s = sc.nextLine();
+        String result= "";
         char[] ch = new char[s.length()];
 
         for (int i = 0; i < s.length(); i++) {
             ch[i] = s.charAt(i);
         }
-        out.print("Good string: ");
         for (int i = 0; i < s.length(); i++) {
             if (ch[i] >= 65 && ch[i] <= 90) {
                 int a = (int) ch[i] + 32;
-                out.print((char) a);
-            } else {
-                out.print(ch[i]);
+                ch[i] = (char)a;
             }
+            result = result + ch[i];
         }
-        out.println("");
+        
+        out.println("Good string: " + result);
     }
 
 }
+
